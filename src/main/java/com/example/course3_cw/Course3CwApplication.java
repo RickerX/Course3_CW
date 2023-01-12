da629e13fd3790743855cd1405e097538de0cef5
roman <roman.pshenichnikov555@gmail.com>
2023-01-12 16:58:01 +0300

    Course3_CW

---
+++ src/main/java/com/example/course3_cw/Course3CwApplication.java
@@ -0,0 +1,13 @@
+package com.example.course3_cw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3CwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3CwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course3_cw/Course3CwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course3_cw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3CwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
